811f4ec4c7487a3007eced391ac7c474ca254408
arthurgauffre <arth.gauffre@gmail.com>
2024-03-15 21:48:19 +0100

    Refactor code to save and load city data and likes

--- smash_or_pass.py
+++ smash_or_pass.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python3
 
 import json
-import sys
 import requests
 
-def print_json_content(json_file):
-    with open(json_file, 'r') as file:
-        data = json.load(file)
-        print(json.dumps(data, indent=4))
+def save_city_data(data):
+    with open('city_data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+def load_city_data():
+    try:
+        with open('city_data.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_likes(likes):
+    with open('city_likes.json', 'w') as file:
+        json.dump(likes, file, indent=4)
+
+def load_likes():
+    try:
+        with open('city_likes.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
 
 def get_random_city():
-    url = 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?where=country_code%20%3D%20%22FR%22%20and%20population%20%3E%2010000&limit=100'
-    response = requests.get(url)
+    url = 'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records'
+    params = {
+        'select': 'name, coordinates, population',
+        'where': 'country_code = "FR" and population > 10000',
+        'limit': 100,
+    }
+    response = requests.get(url, params=params)
     if response.status_code == 200:
         data = response.json()
-        print(data)
+        save_city_data(data)
+        print("City data saved successfully.")
     else:
         print("La requête a échoué avec le code :", response.status_code)
-
 
 if __name__ == "__main__":
     get_random_city()
