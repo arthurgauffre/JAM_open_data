05189c31e948ac2a056df4abfb00c6b84a8c90d3
Alea Chlodnik <achlodnik@gmail.com>
2024-03-16 10:42:46 +0100

    [UPD] change like and x buttons

--- smash_or_pass.py
+++ smash_or_pass.py
@@ -4,7 +4,6 @@ import subprocess
 import pygame
 import sys
 
-
 def generate_city_description(city_name):
     prompt = f"créer une description de max 200 caractères sur la ville de {city_name}."
     command_output = subprocess.run(["ollama", "run", "mistral", prompt], capture_output=True).stdout.decode('utf-8')
