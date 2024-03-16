46eed9705d89d444ee0b19bf7632dd2d7ce49905
OxiiLB <romain.giraud@epitech.eu>
2024-03-16 15:40:54 +0100

    [ADD] images folders and images handling

--- smash_or_pass.py
+++ smash_or_pass.py
@@ -4,7 +4,82 @@ import subprocess
 import pygame
 import sys
 import random
-import json
+import requests
+import os
+import re
+
+def download_image(url, filename):
+    try:
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        images_folder = 'images/'
+
+        filepath = os.path.join(images_folder, filename)
+
+        with open(filepath, 'wb') as file:
+            file.write(response.content)
+
+        print(f"Image downloaded successfully as {filepath}")
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def get_image_content(filename):
+    try:
+        filepath = os.path.join('images/', filename)
+
+        with open(filepath, 'r', encoding='utf-8') as file:
+            content = file.read()
+
+        pattern = r'<meta property="og:image" content="(.+?)">'
+        match = re.search(pattern, content)
+
+        if match:
+            return match.group(1)
+        else:
+            return "No meta tag found in the file."
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def remove_file_from_images(filename):
+    try:
+        filepath = os.path.join('images/', filename)
+
+        if os.path.exists(filepath):
+            os.remove(filepath)
+            print(f"File '{filename}' removed successfully from /images folder.")
+        else:
+            print(f"File '{filename}' does not exist in /images folder.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def get_image_url(article_title):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get("https://en.wikipedia.org/w/api.php", 
+                            params={"action": "parse", 
+                                    "page": article_title,
+                                    "format": "json"},
+                            headers=headers)
+    data = response.json()
+    image_url = "https://upload.wikimedia.org/wikipedia/commons/6/6c/No_image_3x4.svg"
+    
+    if "images" in data["parse"]:
+        for image in data["parse"]["images"]:
+            if image.lower().endswith(".jpg"):
+                image_url = f"https://en.wikipedia.org/wiki/File:{image}"
+                break
+    if image_url == "https://upload.wikimedia.org/wikipedia/commons/6/6c/No_image_3x4.svg":
+        download_image(image_url, "current_image.jpg")
+        return image_url
+    download_image(image_url, f"{article_title}.txt")
+    image_url = get_image_content(f"{article_title}.txt")
+    download_image(image_url, "current_image.jpg")
+    remove_file_from_images(f"{article_title}.txt")
+    return image_url
 
 def generate_city_description(city_name):
     prompt = f"créer une description de max 200 caractères sur la ville de {city_name}."
@@ -66,11 +141,11 @@ def smash_or_pass_launch(width, height, cities_info):
     font = pygame.font.Font(None, 36)  # None indicates default font
     
     # Load like and X button images
-    like_button = pygame.image.load("like.png")
-    x_button = pygame.image.load("x_button.png")
+    like_button = pygame.image.load("images/like.png")
+    x_button = pygame.image.load("images/x_button.png")
 
     # load image logo
-    logo = pygame.image.load("logo.png")
+    logo = pygame.image.load("images/logo.png")
     logo = pygame.transform.scale(logo, (300, 150))
 
     # Scale buttons to desired size
@@ -102,6 +177,11 @@ def smash_or_pass_launch(width, height, cities_info):
     # lines.append(current_line)
     # city_description_surface = [font.render(line, True, (0, 0, 0)) for line in lines]
     y_pos = 0  # Position of the first line
+
+    get_image_url("Poitiers")
+    image = pygame.image.load("images/current_image.jpg")
+    image = pygame.transform.scale(image, (500, 300))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -114,11 +194,13 @@ def smash_or_pass_launch(width, height, cities_info):
                         random_city['Like'] += 1
                         update_file_with_data("cities_info.txt", cities_info)
                         random_city = random.choice(cities_info)
+                        remove_file_from_images("current_image.jpg")
                     elif x_button_rect.collidepoint(mouse_pos):
                         random_city = random.choice(cities_info)
         
         screen.fill((255, 255, 255))  # Fill the screen with white color
         
+        screen.blit(image, (width // 2 - 250, 200))
         # Display buttons at their positions
         screen.blit(like_button, like_button_rect)
 
@@ -138,4 +220,5 @@ def smash_or_pass_launch(width, height, cities_info):
         pygame.display.update()
 
 if __name__ == "__main__":
+    get_image_url("Poitiers")
     smash_or_pass_launch(600, 800, read_file_info("cities_info.txt"))
